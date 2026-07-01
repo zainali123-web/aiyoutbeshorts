a1db3d30563bf27abba5230d921034105469448d
zainali123-web <za3051523@gmail.com>
2026-07-01 10:34:57 +0500

    Update download.py

--- download.py
+++ download.py
@@ -1,8 +1,24 @@
 import os
+import requests
 
-api_key = os.getenv("PEXELS_API_KEY")
+API_KEY = os.getenv("PEXELS_API_KEY")
 
-if api_key:
-    print("✅ Pexels API Key Loaded Successfully")
+headers = {
+    "Authorization": API_KEY
+}
+
+url = "https://api.pexels.com/videos/search?query=funny animals&per_page=1"
+
+response = requests.get(url, headers=headers)
+
+print("Status:", response.status_code)
+
+if response.status_code == 200:
+    data = response.json()
+    if data["videos"]:
+        print("✅ Pexels API Working!")
+        print("Video URL:", data["videos"][0]["video_files"][0]["link"])
+    else:
+        print("No videos found.")
 else:
-    print("❌ Pexels API Key Not Found")
+    print(response.text)
